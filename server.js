c95b3a18791561a8905b00d2a1338b6630335424
Slogreco <shoshwright@hotmail.com>
2020-10-27 10:19:01 -0700

    added server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+require("./routes/apiRoutes");
+require("./routes/htmlRoutes");
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT, http://localhost:" + PORT);
+});
